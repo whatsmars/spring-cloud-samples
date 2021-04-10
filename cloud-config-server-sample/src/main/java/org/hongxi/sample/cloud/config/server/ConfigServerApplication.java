0baaa6482c8801ba1a1df1432a64b67c589043f0
javahongxi <javahongxi@qq.com>
2021-04-11 06:56:13 +0800

    config server sample

---
+++ cloud-config-server-sample/src/main/java/org/hongxi/sample/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package org.hongxi.sample.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by shenhongxi on 2021/4/11.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
