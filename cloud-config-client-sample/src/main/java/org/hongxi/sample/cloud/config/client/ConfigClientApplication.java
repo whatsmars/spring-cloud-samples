eb2583623e7a49384bd8ab3752d5be507d148b83
javahongxi <javahongxi@qq.com>
2021-04-11 12:32:34 +0800

    config & discovery

--- cloud-config-client-sample/src/main/java/org/hongxi/sample/cloud/config/client/ConfigClientApplication.java
+++ cloud-config-client-sample/src/main/java/org/hongxi/sample/cloud/config/client/ConfigClientApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 /**
  * Created by shenhongxi on 2021/4/11.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- cloud-config-server-sample/src/main/java/org/hongxi/sample/cloud/config/server/ConfigServerApplication.java
+++ cloud-config-server-sample/src/main/java/org/hongxi/sample/cloud/config/server/ConfigServerApplication.java
@@ -2,11 +2,13 @@ package org.hongxi.sample.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by shenhongxi on 2021/4/11.
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
